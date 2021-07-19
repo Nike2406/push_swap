fba10a0f146c571af140471511626c0879ca5c5f
prochell <prochell@student.21-school.ru>
2021-07-19 19:24:08 +0300

    add lst_functions-file

--- push_swap.h
+++ push_swap.h
@@ -22,8 +22,15 @@ typedef struct s_ps_struct
 void	get_parse(t_ps_struct *s_psw, t_stack *l_a, t_stack *l_b);
 void	get_sort_ps(t_ps_struct *s_psw, int i, int j);
 void	ft_swap(int *a, int *b);
+
 void	ft_err(int	code);
 void	check_isnum(int argc, char **argv);
 void	check_repeat(t_ps_struct *s_psw);
+
+void	ft_lstadd_back_ps(t_stack **lst, t_stack *new);
+void	ft_lstadd_front_ps(t_stack **lst, t_stack *new);
+void	ft_lstclear_ps(t_stack **lst, void (*del)(void *));
+t_stack	*ft_lstlast_ps(t_stack *lst);
+t_stack	*ft_lstnew_ps(void *content);
 
 #endif
---
+++ src/lst_functions.c
@@ -0,0 +1,67 @@
+#include "../push_swap.h"
+
+void	ft_lstadd_back_ps(t_stack **lst, t_stack *new)
+{
+	t_stack	*tmp;
+
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	tmp = ft_lstlast_ps(*lst);
+	tmp->next = new;
+}
+
+void	ft_lstadd_front_ps(t_stack **lst, t_stack *new)
+{
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	if (!new)
+		return ;
+	new->next = *lst;
+	*lst = new;
+}
+
+void	ft_lstclear_ps(t_stack **lst, void (*del)(void *))
+{
+	t_stack	*tmp;
+
+	if (!lst || !del)
+		return ;
+	while (*lst)
+	{
+		del((*lst)->value);
+		tmp = *lst;
+		*lst = tmp->next;
+		free(tmp);
+	}
+	*lst = NULL;
+}
+
+t_stack	*ft_lstlast_ps(t_stack *lst)
+{
+	t_stack	*tmp;
+
+	tmp = lst;
+	if (!lst)
+		return (NULL);
+	while (tmp->next)
+		tmp = tmp->next;
+	return (tmp);
+}
+
+t_stack	*ft_lstnew_ps(void *content)
+{
+	t_stack	*nel;
+
+	nel = (t_stack *)malloc(sizeof(t_stack));
+	if (!nel)
+		return (NULL);
+	nel->value = content;
+	nel->next = NULL;
+	return (nel);
+}
